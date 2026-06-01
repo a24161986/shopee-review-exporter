7bdaae515bcee297994bab0817aff1ed88fd2848
Jarrwen <24161986@qq.com>
2026-06-01 15:03:04 +0800

    feat: add extension manifest and scanner

---
+++ extension/content/content.js
@@ -0,0 +1,20 @@
+(function attachContentScanner(global) {
+  function collectPageText() {
+    const anchorText = Array.from(document.querySelectorAll('a[href]'))
+      .map((anchor) => anchor.href)
+      .join('\n');
+    return `${anchorText}\n${document.body?.innerText || ''}`;
+  }
+
+  function scanCurrentPage() {
+    const parser = global.ShopeeReviewExporter;
+    const products = parser.extractShopeeProductLinks(collectPageText());
+    return {
+      pageUrl: location.href,
+      pageTitle: document.title,
+      products
+    };
+  }
+
+  global.ShopeeReviewExporter = Object.assign(global.ShopeeReviewExporter || {}, { scanCurrentPage });
+})(typeof globalThis !== 'undefined' ? globalThis : window);
